b03bb7e3f3246247f0efff53845c3c0bd00d3eb3
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-30 23:56:30 +0900

    :+1: Update put link script

--- scripts/putlink.sh
+++ scripts/putlink.sh
@@ -1,8 +1,12 @@
-ln -sf ./dotfiles/gitignore_global ~/.gitignore_global
-ln -sf ./dotfiles/gitmessage ~/.gitmessage
-ln -sf ./dotfiles/zshrc ~/.zshrc
-ln -sf ./dotfiles/.hyper.js ~/.hyper.js
-ln -sf ./dotfiles/vscode/settings.json ~/.vscode/setting.json
+# dotfiles
+ln -sf .gitignore_global ~/.gitignore_global
+ln -sf .gitmessage ~/.gitmessage
+ln -sf .zshrc ~/.zshrc
+ln -sf .hyper.js ~/.hyper.js
+ln -sf .tmux.conf ~/.tmux.conf
+
+# Configuration files
+ln -sf ./vscode/settings.json ~/.vscode/setting.json
 
 source ~/.zshrc
 git config --global core.excludesfile ~/.gitignore_global
