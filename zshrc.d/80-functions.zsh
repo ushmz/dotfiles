cfa724ed9e8827bd009c53a1a8769db886b1f809
Yusuke Shimizu <44394399+rabhareit@users.noreply.github.com>
2021-02-11 16:22:46 +0900

    :sparkles: Add tmux automation

--- zshrc.d/80-functions.zsh
+++ zshrc.d/80-functions.zsh
@@ -29,3 +29,28 @@ function googleit() {
 alias what="googleit"
 
 function itermprof() { echo -ne "\033]1337;SetProfile=$@\a" }
+
+function tmux_session() {
+    if [ -n "$TMUX" ]; then
+        tm "$@"
+	return 0
+    fi
+
+    if [ -n "$1" ]; then
+	if [ -n "$2" ]; then
+	    itermprof "$2"
+	    tmux new -d -s "${1}-${2}"
+	    tmux send-keys -t "${1}-${2}" "tm ${1} ${2}" C-m
+	    tmux attach-session -t "${1}-${2}"
+	    itermprof kahma
+	else
+	    tmux new -d -s $1
+	    tmux send-keys -t $1 "tm $1" C-m
+	    tmux attach-session -t $1
+	fi
+    else
+	tmux
+    fi
+}
+alias mux='tmux_session'
+
