4a48d065a1792847295466164b41a78b71d17aa8
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-10-01 23:05:54 +0900

    :hammer: Update make-up script

--- put.sh
+++ put.sh
@@ -1,42 +1,67 @@
 #!/bin/sh
+set -e
 
+# ----------------------------------------------------------
+# Remove `~/.zprofile` if exists, and write my `.zprofile`.
+# ----------------------------------------------------------
 if [ -L $HOME/.zprofile ]; then
     unlink $HOME/.zprofile
 fi
 echo "#\!/bin/zsh\nfor i in \$(command ls `pwd`/zprofile.d/*.zsh | sort)\ndo\n  source \$i\ndone" > $HOME/.zprofile
 
+# ----------------------------------------------------
 # Remove `~/.zshrc` if exists, and write my `.zshrc`.
+# ----------------------------------------------------
 if [ -L $HOME/.zshrc ]; then
     unlink $HOME/.zshrc
 fi
 echo "#\!/bin/zsh\nfor i in \$(command ls `pwd`/zshrc.d/*.zsh | sort)\ndo\n  source \$i\ndone" > $HOME/.zshrc
 
 # Put symbolic links.
+
+# ------------------------
 # .gitmessage
+# ------------------------
 ln -sf `pwd`/.gitmessage $HOME/.gitmessage
 
-# .gitignore_global and source it.
+# ------------------------
+# .gitignore_global
+# ------------------------
 ln -sf `pwd`/.gitignore_global $HOME/.gitignore_global
 git config --global core.excludesfile $HOME/.gitignore_global	
 
+# ------------------------
 # alacritty config file
+# ------------------------
 mkdir -p $XDG_CONFIG_HOME/alacritty/
 ln -sf `pwd`/.config/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
 
+# ------------------------
 # .tmux.conf
+# ------------------------
 ln -sf `pwd`/.tmux.conf $HOME/.tmux.conf
 
+# ------------------------
 # starship.toml
+# ------------------------
 ln -sf `pwd`/.config/starship.toml $XDG_CONFIG_HOME/starship.toml
 
+# ------------------------
 # .tigrc
+# ------------------------
 ln -sf `pwd`/.tigrc $HOME/.tigrc
 
-# vscode setting file
-mkdir -p "$HOME/.vscode/"
-ln -sf `pwd`/vscode/settings.json $HOME/.vscode/setting.json
-
+# ------------------------
 # nvim config files
+# ------------------------
+# vim color file
+mkdir -p "$XDG_CONFIG_HOME/nvim/colors"
+ln -sf `pwd`/.config/nvim/colors/hybrid.vim $XDG_CONFIG_HOME/nvim/colors/hybrid.vim
+
+# vim skeleton files
+mkdir -p $XDG_CONFIG_HOME/nvim/templates
+ln -sf `pwd`/.config/nvim/templates/skeleton.sh $XDG_CONFIG_HOME/nvim/templates/skeleton.sh
+
 # dein plugin files
 mkdir -p $XDG_CONFIG_HOME/nvim/
 ln -sf `pwd`/.config/nvim/init.vim $XDG_CONFIG_HOME/nvim/init.vim
@@ -46,23 +71,21 @@ ln -sf `pwd`/.config/nvim/dein_lazy.toml $XDG_CONFIG_HOME/nvim/dein_lazy.toml
 # coc setting file
 ln -sf `pwd`/.config/nvim/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
 
-# vim color file
-mkdir -p "$XDG_CONFIG_HOME/nvim/colors"
-ln -sf `pwd`/.config/nvim/colors/hybrid.vim $XDG_CONFIG_HOME/nvim/colors/hybrid.vim
-
 # vim plugin `run command` files
 mkdir -p "$XDG_CONFIG_HOME/nvim/plugins"
-ln -sf `pwd`/.config/nvim/plugins/coc.rc.vim $XDG_CONFIG_HOME/nvim/plugins/coc.rc.vim
-ln -sf `pwd`/.config/nvim/plugins/defx.rc.vim $XDG_CONFIG_HOME/nvim/plugins/defx.rc.vim
-ln -sf `pwd`/.config/nvim/plugins/denite.rc.vim $XDG_CONFIG_HOME/nvim/plugins/denite.rc.vim
-ln -sf `pwd`/.config/nvim/plugins/silicon.rc.vim $XDG_CONFIG_HOME/nvim/plugins/silicon.rc.vim
+for rc in $(ls -I '*.md' `pwd`/.config/nvim/plugins/* | xargs -n 1 basename); do
+    ln -sf `pwd`/.config/nvim/plugins/$rc $XDG_CONFIG_HOME/nvim/plugins/$rc
+done
 
+# ------------------------
+# VSCode config files
+# ------------------------
+mkdir -p "$HOME/.vscode/"
+ln -sf `pwd`/vscode/settings.json $HOME/.vscode/setting.json
+
+# ------------------------
 # Utility scripts
-ln -sf `pwd`/.scripts/cmdnotif.sh $HOME/.scripts/cmdnotif.sh
-ln -sf `pwd`/.scripts/mux.sh $HOME/.scripts/mux.sh
-ln -sf `pwd`/.scripts/typo.sh $HOME/.scripts/typo.sh
-ln -sf `pwd`/.scripts/ssh_change_profile.sh $HOME/.scripts/ssh_change_profile.sh
-ln -sf `pwd`/.scripts/battery $HOME/.scripts/battery
-ln -sf `pwd`/.scripts/load_ave $HOME/.scripts/load_ave
-ln -sf `pwd`/.scripts/ssid $HOME/.scripts/ssid
-
+# ------------------------
+for script in $(ls -I '*.md' `pwd`/.scripts/* | xargs -n 1 basename); do
+    ln -sf `pwd`/.scripts/$script $HOME/.scripts/$script
+done
