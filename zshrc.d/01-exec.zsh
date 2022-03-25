6db19b409e86a9277556395a5f894a0c1b5dafb1
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-03-25 22:07:51 +0900

    :+1: Update rc

--- zshrc.d/01-exec.zsh
+++ zshrc.d/01-exec.zsh
@@ -1,22 +1,10 @@
-##########################################################
 # Start tmux session
-##########################################################
-if [ $SHLVL = 1 ]; then
+if [[  $SHLVL = 1 && $TERM = "alacritty"  ]]; then
   tmux
 fi
 
 # starship (cross-shell prompt)
 eval "$(starship init zsh)"
-
-# java virtual env
-if command -v jenv 1>/dev/null 2>&1; then
-  eval "$(jenv init -)"
-fi
-
-# Elixer virtual env
-if command -v exenv 1>/dev/null 2>&1; then
-  eval "$(exenv init -)"
-fi
 
 # direnv
 export EDITOR=vi
