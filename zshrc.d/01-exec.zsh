803f78f98cc6953b11f1ebd3862ce07cd2f758ac
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-05-12 17:15:26 +0900

    :sparkles: Add conditional shell prompt

--- zshrc.d/01-exec.zsh
+++ zshrc.d/01-exec.zsh
@@ -4,8 +4,16 @@ if [[  $SHLVL = 1 && $TERM = "alacritty"  ]]; then
 fi
 
 # starship (cross-shell prompt)
-if command -v starship > /dev/null; then
+if command -v starship > /dev/null
+then
     eval "$(starship init zsh)"
+else
+    setopt prompt_subst
+    precmd_refresh_prompt() {
+        export PS1=$'\n'"%F{green}%n%f%F{blue}@%f%F{green}%m%f:%~"$'\n'`if [[ $? -eq 0 ]]; then echo %F{white}\>\>\> %f; else echo %F{red}\>\>\> %f; fi`
+    }
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd precmd_refresh_prompt
 fi
 
 # direnv
