0706a610b06a6ef9766948211fd8a80a4bd56f2a
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-05-28 17:23:09 +0900

    :adhesive_bandage: Fix repository URL

--- install.linux.sh
+++ install.linux.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/.cache
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.cache/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.cache/zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-completions.git $HOME/.cache/zsh-completions
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.cache/zsh-completions
 
 chmod 755 $HOME/.cache/zsh-syntax-highlighting
 chmod 755 $HOME/.cache/zsh-autosuggestions
