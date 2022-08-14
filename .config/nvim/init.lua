86d779bdd6689bfe4cd19f6917dfedded1a9f30f
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-08-14 18:23:06 +0900

    :wrench: Configuring with lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,37 @@
+--[[
+if not 1
+    finish
+endif
+--]]
+
+vim.cmd("autocmd!")
+
+-- filetype
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+    pattern = {'*.jsx'},
+    command = 'set filetype=javascriptreact'
+})
+
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+    pattern = {'*.tsx'},
+    command = 'set filetype=typescriptreact'
+})
+
+vim.api.nvim_create_autocmd({'FileType'}, {
+    pattern = {'javascriptreact', 'typescriptreact', 'dart'},
+    command = 'set tabstop=2 shiftwidth=2 softtabstop=2'
+})
+
+-- templates
+vim.api.nvim_create_autocmd({'BufNewFile'}, {
+    pattern = {'*.sh'},
+    command = '0r $HOME/.vim/templates/skeleton.sh'
+})
+
+
+local config_dir = vim.env.XDG_CONFIG_HOME
+vim.api.nvim_set_var('python3_host_prog', config_dir .. '/nvim/pynvim/bin/python')
+
+require('options')
+require('mappings')
+require('plugins')
---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,51 @@
+vim.g.mapleader=" "
+
+local key = vim.keymap
+
+-- key.set('n', '<space>', '<leader>', {noremap=true})
+key.set('i', 'jj', '<ESC>', {noremap=true, silent=true})
+key.set({'n', 'x'}, ';', ':', {noremap=true})
+key.set('n', '<space>w', ':<C-u>w<CR>')
+key.set('n', '<space>q', ':<C-u>q<CR>')
+key.set('n', 'G', 'Gzz')
+key.set('n', 'n', 'nzz')
+key.set('n', 'N', 'Nzz')
+key.set('n', '}', '}zz')
+key.set('n', '{', '{zz')
+key.set('n', 'x', '"_x')
+key.set('n', '<C-d>', '<C-d>zz')
+key.set('n', '<C-d>', '<C-u>zz')
+key.set('n', 'mm', 'ddp', {noremap=true})
+key.set('n', '<space>s', ':<C-u>split<CR><C-w>w', {noremap=true, silent=true})
+key.set('n', '<space>v', ':<C-u>vsplit<CR><C-w>w', {noremap=true, silent=true})
+key.set('n', 'g]', ':<C-u>tabnext<CR>', {noremap=true, silent=true})
+key.set('n', 'g[', ':<C-u>tabprevious<CR>', {noremap=true, silent=true})
+key.set('i', '<C-p>', '<Up>')
+key.set('i', '<C-n>', '<Down>')
+key.set('i', '<C-b>', '<Left>')
+key.set('i', '<C-f>', '<Right>')
+key.set('i', '<C-a>', '<C-o>:call home()<CR>')
+key.set('i', '<C-e>', '<End>')
+key.set('i', '<C-d>', '<Del>')
+key.set('i', '<C-h>', '<BS>')
+key.set('n', '<ESC><ESC>', ':<C-u>nohlsearch<CR>', {silent=true})
+
+-- vp doesn't replace paste buffer
+function RestoreRegister()
+  vim.fn.setreg('"', restore_reg)
+  return ''
+end
+
+function Repl()
+  local restore_reg = vim.fn.getreg('"')
+  return "p@=RestoreRegister()\\<cr>"
+end
+
+key.set('v', 'p', 'Repl()', {silent=true, expr=true})
+
+-- find and replace
+--[[
+vnoremap <silent> s //e<C-r>=&selection=='exclusive'?'+1':''<CR><CR>
+    \:<C-u>call histdel('search',-1)<Bar>let @/=histget('search',-1)<CR>gv
+omap s :normal vs<CR>
+--]]
