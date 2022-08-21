167a2514a1c059ac2e124a49c8a863d762368fdc
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-08-21 18:01:33 +0900

    :lipstick: Update bufferline style

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -5,29 +5,29 @@ bufferline.setup({
   options = {
     mode = 'tabs',
     separator_style = 'slant',
-    always_show_bufferline = false,
+    always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true
   },
   highlights = {
     separator = {
-      fg = '#073642',
-      bg = '#002b36',
+      fg = '#4E4E4E',
+      bg = '#2D2F31',
     },
     separator_selected = {
-      fg = '#073642',
+      fg = '#4E4E4E',
     },
     background = {
-      fg = '#657b83',
-      bg = '#002b36'
+      fg = '#657B83',
+      bg = '#2D2F31'
     },
     buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true
+      fg = '#FDF6E3',
+      italic = false
     },
     fill = {
-      bg = '#073642'
+      bg = '#4E4E4E'
     }
   },
 })
