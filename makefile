3e7b69b0f9289f5e0291589ccba7c56e38b2d24c
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-08-22 14:20:07 +0900

    :shower: Add Okatazuke script

--- makefile
+++ makefile
@@ -3,6 +3,7 @@ define _help
 	echo "put\t: Put all config files(Works on darwin)"
 	echo "up\t: Put all config files and install brew packages"
 	echo "upl\t: Put all config files and install packages (linux)"
+	echo "okatazuke": Unlink & remove all synbolic links
 endef
 
 default:
@@ -25,3 +26,6 @@ up: setup/put.sh setup/install.darwin.sh
 upl: setup/put.sh setup/install.linux.sh
 	sh `pwd`/setup/put.sh
 	sh `pwd`/setup/install.linux.sh
+
+okatazuke: setup/okatazuke.sh
+	sh `pwd`/setup/okatazuke.sh
---
+++ setup/okatazuke.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+unlink ${HOME}/.zprofile
+unlink ${HOME}/.zshrc
+
+unlink ${HOME}/.zshrc
+unlink ${HOME}/.skhdrc
+unlink "${HOME}/Library/Application Support/Code/User/settings.json"
+unlink "${HOME}/Library/Application Support/Code/User/keybindings.json"
+unlink ${XDG_DATA_HOME}/tig/.tigrc
+unlink ${XDG_CONFIG_HOME}/starship.toml
+
+rm -rf ${XDG_CONFIG_HOME}/alacritty
+rm -rf ${XDG_CONFIG_HOME}/tmux
+rm -rf ${XDG_CONFIG_HOME}/git/config
+rm -rf ${XDG_CONFIG_HOME}/gh/config
+rm -rf ${XDG_CONFIG_HOME}/ranger
+rm -rf ${XDG_CONFIG_HOME}/nvim
+rm -rf ${HOME}/.scripts
+
+rm "${XDG_CONFIG_HOME}/wgetrc"
+rm -rf "${XDG_CONFIG_HOME}/python/pythonrc"
