20afeecf9a9e8b304c89761eb33af1c5af025678
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-09-15 16:00:50 +0900

    :sparkles: Follow XDG base directory to clean HOME directory

--- zprofile.d/01-env.zsh
+++ zprofile.d/01-env.zsh
@@ -10,15 +10,29 @@ export XDG_DATA_HOME=${HOME}/.local/share
 export XDG_CACHE_HOME=${HOME}/.cache
 
 # Stop dirtying Home directory
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
+export FFMPEG_DATADIR="${XDG_CONFIG_HOME}/ffmpeg"
 export HISTFILE="${XDG_STATE_HOME}/zsh_history"
-export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node_history"
-export SQLITE_HISTORY="${XDG_STATE_HOME}/sqlite_history"
-export MYSQL_HISTFILE="${XDG_STATE_HOME}/mysql_history"
-export PSQL_HISTORY="${XDG_STATE_HOME}/psql_history"
-export SQLITE_HISTORY="${XDG_DATA_HOME}/sqlite_history"
-export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
 export IPYTHONDIR="${XDG_CONFIG_HOME}/jupyter"
 export JUPYTER_CONFIG_DIR="${XDG_CONFIG_HOME}/jupyter"
+export MYSQL_HISTFILE="${XDG_STATE_HOME}/mysql_history"
+export NODE_REPL_HISTORY="${XDG_STATE_HOME}/node_history"
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
+export NUGET_PACKAGES="${XDG_CACHE_HOME}/NuGetPackages"
+export PSQL_HISTORY="${XDG_STATE_HOME}/psql_history"
+export PYLINTHOME="${XDG_CACHE_HOME}/pylint"
+export PYLINTRC="${XDG_CONFIG_HOME}/pylint/pylintrc"
+export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
+export REDISCLI_HISTFILE="${XDG_DATA_HOME}/redis/rediscli_history"
+export REDISCLI_RCFILE="${XDG_CONFIG_HOME}/redis/redisclirc"
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+export SQLITE_HISTORY="${XDG_STATE_HOME}/sqlite_history"
+export STACK_ROOT="${XDG_DATA_HOME}/stack"
+export STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship.toml"
+export STARSHIP_CACHE="${XDG_CACHE_HOME}/starship"
+export VAGRANT_HOME="${XDG_DATA_HOME}/vagrant"
+export VAGRANT_ALIAS_FILE="${XDG_DATA_HOME}/vagrant/aliases"
 export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
 # export GOPATH="$XDG_DATA_HOME/go"
 
@@ -31,4 +45,4 @@ export PATH=${PATH}:${GOBIN}
 export PGDATA="/usr/local/var/postgres"
 
 # cargo env
-source "${HOME}/.cargo/env"
+source "${CARGO_HOME}/env"
--- zprofile.d/10-path.zsh
+++ zprofile.d/10-path.zsh
@@ -1,9 +1,27 @@
-# Programming languages
-# --------------------------------------------------
+# -U: keep only the first occurrence of each duplicated value
+# ref. http://zsh.sourceforge.net/Doc/Release/Shell-Builtin-Commands.html#index-typeset
+typeset -U PATH path MANPATH manpath
+
+# ignore /etc/zprofile, /etc/zshrc, /etc/zlogin, and /etc/zlogout
+# ref. http://zsh.sourceforge.net/Doc/Release/Files.html
+# ref. http://zsh.sourceforge.net/Doc/Release/Options.html#index-GLOBALRCS
+unsetopt GLOBAL_RCS
+
 export ANDROID_SDK_HOME=${HOME}/Library/Android/sdk
 export ANDROID_HOME=${HOME}/Library/Android/sdk
 
-path+=(
+path=(
+    # brew
+    "/usr/local/bin"(N-/)
+    "/usr/local/sbin"(N-/)
+    # GNU/Linux commands
+    "/usr/local/opt/coreutils/libexec/gnubin"(N-/) # coreutils
+    "/usr/local/opt/ed/libexec/gnubin"(N-/) # ed
+    "/usr/local/opt/findutils/libexec/gnubin"(N-/) # findutils
+    "/usr/local/opt/gnu-sed/libexec/gnubin"(N-/) # sed
+    "/usr/local/opt/gnu-tar/libexec/gnubin"(N-/) # tar
+    "/usr/local/opt/grep/libexec/gnubin"(N-/) # grep
+
     # Original scripts
     "$HOME/.scripts"(N-/)
     # local packages
@@ -29,4 +47,23 @@ path+=(
     # Remote server
     "/usr/local/pyenv/shims"(N-/)
     "/usr/local/pyenv/bin"(N-/)
+    ${path}
+)
+
+manpath=(
+    #brew
+    "/usr/local/share/man"(N-/)
+    # coreutils
+    "/usr/local/opt/coreutils/libexec/gnuman"(N-/)
+    # ed
+    "/usr/local/opt/ed/libexec/gnuman"(N-/)
+    # findutils
+    "/usr/local/opt/findutils/libexec/gnuman"(N-/)
+    # sed
+    "/usr/local/opt/gnu-sed/libexec/gnuman"(N-/)
+    # tar
+    "/usr/local/opt/gnu-tar/libexec/gnuman"(N-/)
+    # grep
+    "/usr/local/opt/grep/libexec/gnuman"(N-/)
+    ${manpath}
 )
