646bdb334e60b1e4941cea1ba8c36b2efb8197c5
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-10-05 20:10:06 +0900

    :v: Add custom key-bindings

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,6 +2,10 @@ local ok, comment = pcall(require, 'Comment')
 if (not ok) then return end
 
 comment.setup({
+  toggler = {
+    line = '<Leader>c',
+    block = '<Leader>b',
+  },
   opleader = {
     line = '<Leader>c',
     block = '<Leader>b',
