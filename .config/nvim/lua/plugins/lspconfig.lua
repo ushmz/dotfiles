98c5f6c5e8d614a6407b690a3ccd6a79f377135c
ushmz <44394399+ushmz@users.noreply.github.com>
2023-01-22 17:12:07 +0900

    feat: use null-ls on formatting

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,187 +1,174 @@
-local status1, lsp = pcall(require, 'lspconfig')
-if (not status1) then return end
+local status1, lsp = pcall(require, "lspconfig")
+if not status1 then
+	return
+end
 
-local status2, ml = pcall(require, 'mason-lspconfig')
-if (not status2) then return end
+local status2, ml = pcall(require, "mason-lspconfig")
+if not status2 then
+	return
+end
 
-
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  underline = true,
-  update_in_insert = true,
-  float = {
-    source = 'if_many',
-  },
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = true,
+	float = {
+		source = "if_many",
+	},
 })
 
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics,
-  {
-    underline = true,
-    update_in_insert = false,
-    virtual_text = false,
-    severity_sort = true,
-  }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	update_in_insert = false,
+	virtual_text = false,
+	severity_sort = true,
+})
 
 local on_attach = function(client, bufnr)
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-  local opts = { noremap = true, silent = true }
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gt', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	local opts = { noremap = true, silent = true }
+	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "gt", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 
-  local fmt_fnstr = '<Cmd>lua vim.lsp.buf.format({bufnr = ' .. bufnr .. ', timeout_ms = 10000})<CR>'
+	if client.server_capabilities.documentFormattingProvider then
+		buf_set_keymap("n", "<leader>f", "<Cmd>lua vim.lsp.buf.format({timeout_ms = 5000})<CR>", opts)
+	end
 
-  buf_set_keymap('n', '<leader>f', fmt_fnstr, opts)
-
-  if client.server_capabilities.documentHighlightProvider then
-    vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
-    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      callback = vim.lsp.buf.document_highlight,
-      buffer = bufnr,
-      group = 'lsp_document_highlight',
-    })
-    vim.api.nvim_create_autocmd({ 'Cursormoved' }, {
-      callback = vim.lsp.buf.clear_references,
-      buffer = bufnr,
-      group = 'lsp_document_highlight',
-    })
-    vim.api.nvim_set_hl(0, 'LspReferenceText', { ctermbg = 240, bg = '#515761' })
-    vim.api.nvim_set_hl(0, 'LspReferenceRead', { ctermbg = 240, bg = '#515761' })
-    vim.api.nvim_set_hl(0, 'LspReferenceWrite', { ctermbg = 240, bg = '#515761' })
-  end
+	if client.server_capabilities.documentHighlightProvider then
+		vim.api.nvim_create_augroup("LspDocumentHighlight", { clear = true })
+		vim.api.nvim_clear_autocmds({ group = "LspDocumentHighlight" })
+		vim.api.nvim_create_autocmd({ "CursorHold" }, {
+			callback = vim.lsp.buf.document_highlight,
+			group = "LspDocumentHighlight",
+		})
+		vim.api.nvim_create_autocmd({ "Cursormoved" }, {
+			callback = vim.lsp.buf.clear_references,
+			group = "LspDocumentHighlight",
+		})
+		vim.api.nvim_set_hl(0, "LspReferenceText", { ctermbg = 240, bg = "#515761" })
+		vim.api.nvim_set_hl(0, "LspReferenceRead", { ctermbg = 240, bg = "#515761" })
+		vim.api.nvim_set_hl(0, "LspReferenceWrite", { ctermbg = 240, bg = "#515761" })
+	end
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local on_attach_with_null_ls = function(client, bufnr)
+	client.server_capabilities.documentHighlightProvider = false
+	client.server_capabilities.documentFormattingProvider = false
+	on_attach(client, bufnr)
+end
 
-local lsp_flags = {
-  debounce_text_changes = 150,
+local custom_servers = {
+	["sumneko_lua"] = {
+		prefer_null_ls = true,
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+			},
+		},
+	},
+	["jsonls"] = {
+		prefer_null_ls = true,
+		settings = {
+			json = {
+				schemas = {
+					{
+						fileMatch = { "package.json" },
+						url = "https://json.schemastore.org/package.json",
+					},
+					{
+						fileMatch = { "tsconfig*.json" },
+						url = "https://json.schemastore.org/tsconfig.json",
+					},
+					{
+						fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
+						url = "https://json.schemastore.org/prettierrc.json",
+					},
+					{
+						fileMatch = { ".eslintrc", ".eslintrc.json" },
+						url = "https://json.schemastore.org/eslintrc.json",
+					},
+					{
+						fileMatch = { ".stylelintrc", ".stylelintrc.json", "stylelint.config.json" },
+						url = "http://json.schemastore.org/stylelintrc.json",
+					},
+					{
+						fileMatch = { "/.github/workflows/*" },
+						url = "https://json.schemastore.org/github-workflow.json",
+					},
+				},
+			},
+		},
+	},
+	["stylelint_lsp"] = {
+		prefer_null_ls = true,
+		filetypes = { "css", "less", "scss", "sugarss", "vue", "wxss" },
+	},
 }
 
-ml.setup_handlers({
-  function(server_name)
-    lsp[server_name].setup({
-      on_attach = on_attach,
-      flags = lsp_flags,
-      capabilities = capabilities,
-    })
-  end,
-  ['sumneko_lua'] = function(_)
-    lsp.sumneko_lua.setup {
-      on_attach = on_attach,
-      flags = lsp_flags,
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' },
-          },
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-          workspace = {
-            library = vim.api.nvim_get_runtime_file('', true),
-            checkThirdParty = false
-          },
-        },
-      },
-    }
-  end,
-  ['jsonls'] = function(_)
-    lsp.jsonls.setup({
-      on_attach = on_attach,
-      flags = lsp_flags,
-      capabilities = capabilities,
-      settings = {
-        json = {
-          schemas = {
-            {
-              fileMatch = { "package.json" },
-              url = "https://json.schemastore.org/package.json",
-            },
-            {
-              fileMatch = { "tsconfig*.json" },
-              url = "https://json.schemastore.org/tsconfig.json",
-            },
-            {
-              fileMatch = {
-                ".prettierrc",
-                ".prettierrc.json",
-                "prettier.config.json",
-              },
-              url = "https://json.schemastore.org/prettierrc.json",
-            },
-            {
-              fileMatch = { ".eslintrc", ".eslintrc.json" },
-              url = "https://json.schemastore.org/eslintrc.json",
-            },
-            {
-              fileMatch = {
-                ".stylelintrc",
-                ".stylelintrc.json",
-                "stylelint.config.json",
-              },
-              url = "http://json.schemastore.org/stylelintrc.json",
-            },
-            {
-              fileMatch = { "/.github/workflows/*" },
-              url = "https://json.schemastore.org/github-workflow.json",
-            },
-          }
-        }
-      }
-    })
-  end,
-  ['stylelint_lsp'] = function(_)
-    lsp.stylelint_lsp.setup({
-      on_attach = on_attach,
-      flags = lsp_flags,
-      capabilities = capabilities,
-      filetypes = {
-        "css",
-        "less",
-        "scss",
-        "sugarss",
-        "vue",
-        "wxss",
-      },
-    })
-  end
+ml.setup_handlers({
+	function(server_name)
+		local config = {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		}
+		if custom_servers[server_name] then
+			config = custom_servers[server_name]
+			if custom_servers[server_name]["prefer_null_ls"] then
+				config.on_attach = on_attach_with_null_ls
+			else
+				config.on_attach = on_attach
+			end
+		end
+		lsp[server_name].setup(config)
+	end,
 })
 
 -- mason doesn't have `dartls` install option
-lsp.dartls.setup {
-  on_attach = on_attach,
-  flags = lsp_flags,
-  capabilities = capabilities,
-  cmd = { 'dart', 'language-server', '--protocol=lsp' },
-  filetypes = { 'dart' },
-  root_pattern = { 'pubspec.yaml' },
-  init_options = {
-    closingLabels = true,
-    flutterOutline = true,
-    onlyAnalyzeProjectsWithOpenFiles = true,
-    outline = true,
-    suggestFromUnimportedLibraries = true
-  },
-  settings = {
-    dart = {
-      completeFunctionCalls = true,
-      showTodos = true
-    }
-  },
-}
+lsp.dartls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "dart", "language-server", "--protocol=lsp" },
+	filetypes = { "dart" },
+	root_pattern = { "pubspec.yaml" },
+	init_options = {
+		closingLabels = true,
+		flutterOutline = true,
+		onlyAnalyzeProjectsWithOpenFiles = true,
+		outline = true,
+		suggestFromUnimportedLibraries = true,
+	},
+	settings = {
+		dart = {
+			completeFunctionCalls = true,
+			showTodos = true,
+		},
+	},
+})
--- .config/nvim/lua/plugins/nullls.lua
+++ .config/nvim/lua/plugins/nullls.lua
@@ -1,68 +1,95 @@
-local status1, nls = pcall(require, 'null-ls')
-if (not status1) then return end
+local status1, nls = pcall(require, "null-ls")
+if not status1 then
+	return
+end
 
-local status2, mp = pcall(require, 'mason-core.package')
-if (not status2) then return end
+local status2, mp = pcall(require, "mason-core.package")
+if not status2 then
+	return
+end
 
-local status3, mr = pcall(require, 'mason-registry')
-if (not status3) then return end
+local status3, mr = pcall(require, "mason-registry")
+if not status3 then
+	return
+end
 
 local nls_sources = {}
 
 for _, package in ipairs(mr.get_installed_packages()) do
-  local categories = package.spec.categories[1]
-  if categories == mp.Cat.Formatter then
-    table.insert(nls_sources, nls.builtins.formatting[package.name])
-  end
-  if categories == mp.Cat.Linter then
-    table.insert(nls_sources, nls.builtins.diagnostics[package.name])
-  end
+	local categories = package.spec.categories[1]
+	if categories == mp.Cat.Formatter then
+		table.insert(nls_sources, nls.builtins.formatting[package.name])
+	end
+	if categories == mp.Cat.Linter then
+		table.insert(nls_sources, nls.builtins.diagnostics[package.name])
+	end
 end
 
 -- Following tools cannot install via `ensure_installed` option of mason,
 local ext_fmt = {
-  'dart_format',
-  'ktlint',
-  'markdownlint',
-  'rubocop',
-  'black',
-  'goimports',
-  'prettier',
-  'shfmt',
-  'yamlfmt',
+	"dart_format",
+	"ktlint",
+	"markdownlint",
+	"rubocop",
+	"black",
+	"goimports",
+	"prettier",
+	"shfmt",
+	"yamlfmt",
 }
 
 local ext_diag = {
-  'flake8',
-  'rubocop',
+	"flake8",
+	"rubocop",
 }
 
 for _, fmt in ipairs(ext_fmt) do
-  table.insert(nls_sources, nls.builtins.formatting[fmt])
+	table.insert(nls_sources, nls.builtins.formatting[fmt])
 end
 
 for _, diag in ipairs(ext_diag) do
-  table.insert(nls_sources, nls.builtins.diagnostics[diag])
+	table.insert(nls_sources, nls.builtins.diagnostics[diag])
 end
 
-nls.setup({
-  sources = nls_sources,
-  on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      local fmtag = vim.api.nvim_create_augroup('LspDocumentFormatting', {})
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = fmtag })
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = bufnr,
-        group = fmtag,
-        callback = function()
-          vim.lsp.buf.format({
-            bufnr = bufnr,
-            filter = function(clt)
-              return clt.name == 'null-ls'
-            end,
-          })
-        end,
-      })
+-- Not work!!
+--[[
+local fmt_callback_str = [[
+	<Cmd>lua vim.lsp.buf.format({
+    timeout_ms = 5000,
+    bufnr = bufnr,
+    filter = function(clt)
+      return clt.name == 'null-ls'
     end
-  end
+  })<CR>
+]]
+-- Above line is not a comment!
+
+nls.setup({
+	sources = nls_sources,
+	on_attach = function(client, bufnr)
+		if client.server_capabilities.documentFormattingProvider then
+			vim.api.nvim_buf_set_keymap(
+				bufnr,
+				"n",
+				"<leader>f",
+				"<Cmd>lua vim.lsp.buf.format({ timeout_ms = 5000, bufnr = bufnr, filter = function(clt) return clt.name == 'null-ls' end })<CR>",
+				{ noremap = true, silent = true }
+			)
+
+			local fmtag = vim.api.nvim_create_augroup("LspDocumentFormatting", {})
+			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = fmtag })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				buffer = bufnr,
+				group = fmtag,
+				callback = function()
+					vim.lsp.buf.format({
+						bufnr = bufnr,
+						filter = function(clt)
+							return clt.name == "null-ls"
+						end,
+					})
+				end,
+			})
+		end
+	end,
 })
