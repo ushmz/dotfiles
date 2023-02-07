93044fff28e8c256edd25e839ab09096e1bfa796
ushmz <44394399+ushmz@users.noreply.github.com>
2023-02-08 01:00:14 +0900

    feat: add brewfile export script

---
+++ setup/brewfile.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -Cue
+
+if !(type brew &>/dev/null); then
+	echo 'command brew not found'
+fi
+
+if !(type jq &>/dev/null); then
+	echo 'command jq not found'
+fi
+
+for formula in $(brew list --formula); do
+	if [ ${formula} = "tailscale" ]; then
+		echo brew \'${formula}\', restart_service: true
+	fi
+
+	info=$(brew info ${formula} --formula --json=v2)
+	if [ $(echo ${info} | tr -d '[:cntrl:]' | jq .formulae[0].installed[0].installed_on_request) = "true" ]; then
+		tap=$(echo ${info} | tr -d '[:cntrl:]' | jq .formulae[0].tap)
+		if [ -n "${tap}" ] && [ "${tap}" != '"homebrew/core"' ]; then
+            echo tap $(echo ${tap} | sed -r "s/\\\"/\\'/g")
+		fi
+		echo brew \'${formula}\'
+	fi
+done
+
+for cask in $(brew list --cask); do
+	echo cask \'${cask}\'
+done
