6b673a38a0a7cfd1c0efcd49ec2791af57e7f477
ushmz <44394399+ushmz@users.noreply.github.com>
2023-02-14 00:32:58 +0900

    feat: update Brewfile and export command

--- setup/brewfile.sh
+++ setup/brewfile.sh
@@ -2,27 +2,24 @@
 
 if !(type brew &>/dev/null); then
 	echo 'command brew not found'
+    exit 1
 fi
 
-if !(type jq &>/dev/null); then
-	echo 'command jq not found'
+if !(type mas &>/dev/null); then
+    echo 'command mas not found'
+    exit 1
 fi
 
-for formula in $(brew list --formula); do
-	if [ ${formula} = "tailscale" ]; then
-		echo brew \'${formula}\', restart_service: true
-	fi
+for tap in $(brew tap-info --installed | grep ' formula' | cut -d ':' -f 1); do
+    echo tap \'${tap}\'
+done
 
-	info=$(brew info ${formula} --formula --json=v2)
-	if [ $(echo ${info} | tr -d '[:cntrl:]' | jq .formulae[0].installed[0].installed_on_request) = "true" ]; then
-		tap=$(echo ${info} | tr -d '[:cntrl:]' | jq .formulae[0].tap)
-		if [ -n "${tap}" ] && [ "${tap}" != "null" ] && [ "${tap}" != '"homebrew/core"' ]; then
-            echo tap $(echo ${tap} | sed -r "s/\\\"/\\'/g")
-		fi
-		echo brew \'${formula}\'
-	fi
+for formula in $(brew leaves); do
+    echo brew \'${formula}\'
 done
 
 for cask in $(brew list --cask); do
 	echo cask \'${cask}\'
 done
+
+mas list | sed 's/\ /\ \t#/' | sed 's/\ \ /\ /' | sed 's/\(.\)\s*(/\1 (/' | sed 's/^/mas /'
