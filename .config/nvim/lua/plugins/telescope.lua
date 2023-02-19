890ecb04ca49ef8e0a52728a687bfa47e6158af4
ushmz <44394399+ushmz@users.noreply.github.com>
2023-02-19 18:26:54 +0900

    refactor: add keybind description

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,11 @@
----Plugin configs.
+--- Plugin configs.
 ---@type { config: function, setup: function}
 local M = {}
 
 M.setup = function()
-	---Return telescope builtin with option.
-	---@param name string
-	---@return function
+	--- Return telescope builtin with option.
+	---@param name string: builtin command name
+	---@return fun(opt: table<string, any>): fun()
 	local function builtin(name)
 		return function(opt)
 			return function()
@@ -14,10 +14,10 @@ M.setup = function()
 		end
 	end
 
-	---Return load_extension with option.
-	---@param name string
-	---@param prop string
-	---@return function
+	--- Return load_extension with option.
+	---@param name string: Telescope extension module name
+	---@param prop string: Telescope extension command name
+	---@return fun(opt: table<string, any>): fun()
 	local function extensions(name, prop)
 		return function(opt)
 			return function()
@@ -27,32 +27,42 @@ M.setup = function()
 			end
 		end
 	end
-	vim.keymap.set("n", ";b", builtin("buffers")({}))
-	vim.keymap.set("n", ";c", builtin("commands")({}))
-	vim.keymap.set("n", ";e", builtin("diagnostics")({}))
-	vim.keymap.set("n", ";h", builtin("help_tags")({}))
-	vim.keymap.set("n", ";k", builtin("keymaps")({}))
-	vim.keymap.set("n", ";l", builtin("oldfiles")({}))
-	vim.keymap.set("n", ";;", builtin("resume")({}))
+
+	---@type {desc: string}
+	local opt = {}
+	opt = { desc = "Lists open buffers in current neovim instance, opens selected buffer on `<cr>`" }
+	vim.keymap.set("n", ";b", builtin("buffers")({}), opt)
+
+	opt = { desc = "Lists available plugin/user commands and runs them on `<cr>`" }
+	vim.keymap.set("n", ";c", builtin("commands")({}), opt)
+
+	opt = { desc = "Lists diagnostics" }
+	vim.keymap.set("n", ";e", builtin("diagnostics")({}), opt)
+
+	opt = { desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" }
+	vim.keymap.set("n", ";h", builtin("help_tags")({}), opt)
+
+	opt = { desc = "Lists normal mode keymappings, runs the selected keymap on `<cr>`" }
+	vim.keymap.set("n", ";k", builtin("keymaps")({}), opt)
+
+	opt = { desc = "Lists previously open files, opens on `<cr>`" }
+	vim.keymap.set("n", ";l", builtin("oldfiles")({}), opt)
+
+	opt = { desc = "Opens the previous picker in the identical state (incl. multi selections)" }
+	vim.keymap.set("n", ";;", builtin("resume")({}), opt)
 
 	vim.keymap.set(
 		"n",
 		";f",
-		builtin("find_files")({
-			no_ignore = false,
-			grouped = true,
-			hidden = true,
-		})
+		builtin("find_files")({ no_ignore = false, grouped = true, hidden = true }),
+		{ desc = "Search for files (respecting .gitignore)" }
 	)
 
 	vim.keymap.set(
 		"n",
 		";g",
-		builtin("live_grep")({
-			no_ignore = false,
-			hidden = true,
-			grouped = true,
-		})
+		builtin("live_grep")({ no_ignore = false, hidden = true, grouped = true }),
+		{ desc = "Search for a string and get results live as you type, respects .gitignore" }
 	)
 
 	vim.keymap.set(
@@ -67,12 +77,16 @@ M.setup = function()
 			previewer = false,
 			initial_mode = "normal",
 			layout_config = { height = 40 },
-		})
+		}),
+		{ desc = "Telescope file browser extension" }
 	)
 end
 
 local max_size = 100000
 --- Ignore files bigger than a threshold
+---@param filepath string
+---@param bufnr number
+---@param opts table
 local function new_maker(filepath, bufnr, opts)
 	opts = opts or {}
 	filepath = vim.fn.expand(filepath)
@@ -90,7 +104,7 @@ local function new_maker(filepath, bufnr, opts)
 end
 
 M.config = function()
-  local telescope = require("telescope")
+	local telescope = require("telescope")
 	local actions = require("telescope.actions")
 	local fb_actions = require("telescope").extensions.file_browser.actions
 
@@ -135,6 +149,7 @@ M.config = function()
 				theme = "dropdown",
 				-- disables netrw and use telescope-file-browser in its place
 				hijack_netrw = true,
+				git_status = false,
 				mappings = {
 					["i"] = {
 						["<C-w>"] = function()
