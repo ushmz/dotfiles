79705899caad5260d24d64ad2a5de26f1d7655bb
ushmz <44394399+ushmz@users.noreply.github.com>
2023-03-05 14:02:58 +0900

    refactor(zsh): remove unused code

--- zshrc.d/41-functions_tmux.zsh
+++ zshrc.d/41-functions_tmux.zsh
@@ -42,32 +42,11 @@ function split_tmux_pane() {
 			tmux select-pane -D
 			clear
 			;;
-		"pro")
-			tmux split-window -h
-			tmux split-window -v
-			tmux resize-pane -D 15
-			tmux select-pane -t 1
-			tmux split-window -v
-			tmux select-pane -D
-			clear
-			;;
 		"remote")
 			tmux split-window -h
 			tmux split-window -v
 			tmux resize-pane -D 15
 			tmux select-pane -t 0
-			clear
-			if [ -n "$2" ]; then
-				/usr/bin/ssh "$2"
-			fi
-			;;
-		"xremote")
-			tmux split-window -h
-			tmux split-window -v
-			tmux resize-pane -D 15
-			tmux select-pane -t 0
-			tmux split-window -v
-			tmux select-pane -D
 			clear
 			if [ -n "$2" ]; then
 				/usr/bin/ssh "$2"
