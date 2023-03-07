6932361905b0902a4e1a4fb1b468e088ee3e4595
ushmz <44394399+ushmz@users.noreply.github.com>
2023-03-08 01:43:05 +0900

    feat(script): add chore task script

---
+++ scripts/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh -Cue
+
+function info() {
+    echo -e "\033[0;34m- ${$1}\033[0m"
+}
+
+# Setup asdf
+asdf plugin add java
+asdf plugin add nodejs
+asdf plugin add python
+asdf plugin add ruby
+
+# Install them as necessary
+# asdf install nodejs 14.17.1
+# asdf install nodejs 16.17.0
+# asdf install python 3.6.15
+# asdf install python 3.8.16
+# asdf install python 3.11.2
+# asdf install ruby 3.0.3
+
+# Generate SSH key
+info "SSH setup"
+SSH_KEY_PATH="${HOME}/.ssh"
+if [ ! -e "${SSH_KEY_PATH}/id_rsa.pub" ]; then
+    install -m 0700 -d ${SSH_KEY_PATH}
+    ssh-keygen  -N '' -b 4096 -f ${SSH_KEY_PATH}/id_rsa
+fi
+
+# Set SSH key to github
+pbcopy < ${SSH_KEY_PATH}/id_rsa.pub
+info "SSH key copied to clipboard. Paste it into Github setting."
+sleep 1; open 'https://github.com/settings/ssh/new'
