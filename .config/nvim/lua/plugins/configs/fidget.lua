e84919649711c1bf59a1828fb0f4094a18e4e2fd
ushmz <44394399+ushmz@users.noreply.github.com>
2023-03-19 00:54:16 +0900

    feat(nvim): add setup option (fidget)

--- .config/nvim/lua/plugins/configs/fidget.lua
+++ .config/nvim/lua/plugins/configs/fidget.lua
@@ -1,5 +1,49 @@
 local function config()
-	require("fidget").setup({})
+	require("fidget").setup({
+		text = {
+			spinner = "pipe",
+			done = "✔",
+			commenced = "Started",
+			completed = "Completed",
+		},
+		align = {
+			bottom = true,
+			right = true,
+		},
+		timer = {
+			spinner_rate = 125, -- in ms
+			fidget_decay = 1000, -- in ms
+			task_decay = 500, -- in ms
+		},
+		window = {
+			relative = "win",
+			blend = 0,
+			zindex = nil,
+			border = "none",
+		},
+		fmt = {
+			leftpad = true,
+			stack_upwards = true,
+			max_width = 0,
+			-- function to format fidget title
+			fidget = function(fidget_name, spinner)
+				return string.format("%s %s", spinner, fidget_name)
+			end,
+			-- function to format each task line
+			task = function(task_name, message, percentage)
+				return string.format(
+					"%s%s [%s]",
+					message,
+					percentage and string.format(" (%s%%)", percentage) or "",
+					task_name
+				)
+			end,
+		},
+		debug = {
+			logging = false,
+			strict = false,
+		},
+	})
 end
 
 return {
