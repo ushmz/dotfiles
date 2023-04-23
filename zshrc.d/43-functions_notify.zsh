ee1b5c372f47b4ce0be02daa824ee0ba2506ade5
ushmz <44394399+ushmz@users.noreply.github.com>
2023-04-23 22:08:04 +0900

    refactor(zsh): return early if it is the case

--- zshrc.d/43-functions_notify.zsh
+++ zshrc.d/43-functions_notify.zsh
@@ -1,47 +1,50 @@
-if [ -z "${SLACK_NOTIFY_WEBHOOK_URL+x}" ]; then
-	print -P "\[%F{yellow}WARN%f\] slack_notify: SLACK_NOTIFY_WEBHOOK_URL is empty."
-fi
+if [ -n "${SLACK_NOTIFY_WEBHOOK_URL+x}" ]; then
+	function notify_preexec {
+		notif_prev_executed_at=$(date)
+		notif_prev_command=$2
+	}
 
-if [ -z "${SLACK_NOTIFY_USER_NAME+x}" ]; then
-	print -P "\[%F{yellow}WARN%f\] slack_notify: SLACK_NOTIFY_USER_NAME is empty."
-fi
+	function notify_precmd {
+		if [ ${TTYIDLE} -lt ${SLACK_NOTIF_THRESHOLD:-180} ]; then
+			return
+		fi
 
-function notify_preexec {
-	notif_prev_executed_at=$(date)
-	notif_prev_command=$2
-}
+		notif_status=$?
+		case ${notif_status} in
+		128 | 129 | 130 | 146)
+			return
+			;;
+		esac
 
-function notify_precmd {
-	cmd=$(echo ${notif_prev_command} | awk '{print $1}')
-	subcmd=$(echo ${notif_prev_command} | awk '{print $2}')
+		cmd=$(echo ${notif_prev_command} | awk '{print $1}')
+		subcmd=$(echo ${notif_prev_command} | awk '{print $2}')
 
-	# Define blacklist
-	if [ "${cmd}" = 'uvicorn' ] ||
-		([ "${cmd}" = 'air' ]) ||
-		([ "${cmd}" = 'go' ] && [ "${subcmd}" = 'run' ]) ||
-		([ "${cmd}" = 'cargo' ] && [ "${subcmd}" = 'watch' ]) ||
-		([ "${cmd}" = 'gatsby' ] && [ "${subcmd}" = 'develop' ]) ||
-		([ "${cmd}" = 'flutter' ] && [ "${subcmd}" = 'run' ]) ||
-		([ "${cmd}" = 'npm' ] && [ "${subcmd}" = 'start' ]) ||
-		([ "${cmd}" = 'yarn' ] && [ "${subcmd}" = 'dev' ]) ||
-		([ "${cmd}" = 'yarn' ] && [ "${subcmd}" = 'start' ]); then
-		return
-	fi
+		# Define blacklist
+		if [ "${cmd}" = 'uvicorn' ] ||
+			([ "${cmd}" = 'air' ]) ||
+			([ "${cmd}" = 'go' ] && [ "${subcmd}" = 'run' ]) ||
+			([ "${cmd}" = 'cargo' ] && [ "${subcmd}" = 'watch' ]) ||
+			([ "${cmd}" = 'gatsby' ] && [ "${subcmd}" = 'develop' ]) ||
+			([ "${cmd}" = 'flutter' ] && [ "${subcmd}" = 'run' ]) ||
+			([ "${cmd}" = 'npm' ] && [ "${subcmd}" = 'start' ]) ||
+			([ "${cmd}" = 'yarn' ] && [ "${subcmd}" = 'dev' ]) ||
+			([ "${cmd}" = 'yarn' ] && [ "${subcmd}" = 'start' ]); then
+			return
+		fi
 
-	notif_status=$?
-	if [ -n "${SLACK_NOTIFY_WEBHOOK_URL+x}" ] && [ -n "${SLACK_NOTIFY_USER_NAME+x}" ] && [ ${TTYIDLE} -gt ${SLACK_NOTIF_THRESHOLD:-180} ] && [ ${notif_status} -ne 128 ] && [ ${notif_status} -ne 129 ] && [ ${notif_status} -ne 130 ] && [ ${notif_status} -ne 146 ]; then
 		notif_title=$([ ${notif_status} -eq 0 ] && echo "Command succeeded :ok_woman:" || echo "Command failed :no_good:")
+		notif_text=$([ -n "${SLACK_NOTIFY_USER_NAME+x}" ] && echo "<@${SLACK_NOTIFY_USER_NAME}> ${notif_title}" || echo "${notif_title}")
 		notif_color=$([ ${notif_status} -eq 0 ] && echo "good" || echo "danger")
 		payload=$(
 			cat <<EOS
 {
   "username": "command result",
   "icon_emoji": "${notif_icon}",
-  "text": "<@${SLACK_NOTIFY_USER_NAME}>",
+  "text": "${notif_text}",
   "attachments": [
     {
       "color": "${notif_color}",
-      "title": "${notif_title}",
+      "title": "Description",
       "mrkdwn_in": ["fields"],
       "fields": [
         {
@@ -84,10 +87,10 @@ EOS
 			--header 'Content-type: application/json' \
 			--data "$(echo "${payload}" | tr '\n' ' ' | tr -s ' ')" \
 			${SLACK_NOTIFY_WEBHOOK_URL}
-	fi
-}
+	}
 
-autoload -Uz add-zsh-hook
+	autoload -Uz add-zsh-hook
 
-add-zsh-hook preexec notify_preexec
-add-zsh-hook precmd notify_precmd
+	add-zsh-hook preexec notify_preexec
+	add-zsh-hook precmd notify_precmd
+fi
