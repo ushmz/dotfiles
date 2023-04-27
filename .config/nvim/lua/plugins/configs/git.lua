6bd947d824ff146c21a82f8bb2e42a5a6ad862d4
ushmz <44394399+ushmz@users.noreply.github.com>
2023-04-28 01:02:33 +0900

    feat(nvim): add plugin for git operations

---
+++ .config/nvim/lua/plugins/configs/git.lua
@@ -0,0 +1,31 @@
+local function config()
+	require("git").setup({
+		keymaps = {
+			-- Open blame window
+			blame = "<Leader>gb",
+			-- Close blame window
+			quit_blame = "q",
+			-- Open blame commit
+			blame_commit = "<CR>",
+			-- Open file/folder in git repository
+			browse = "<Leader>go",
+			-- Open pull request of the current branch
+			open_pull_request = "<Leader>gp",
+			-- Create a pull request with the target branch is set in the `target_branch` option
+			create_pull_request = "<Leader>gn",
+			-- Opens a new diff that compares against the current index
+			diff = "<Leader>gd",
+			-- Close git diff
+			diff_close = "<Leader>gD",
+			-- Revert to the specific commit
+			revert = "<Leader>gr",
+			-- Revert the current file to the specific commit
+			revert_file = "<Leader>gR",
+		},
+	})
+end
+
+return {
+	"dinhhuy258/git.nvim",
+	config = config,
+}
