c9b339b5be09187e98ec9e2359a57fc281a5ea68
ushmz <44394399+ushmz@users.noreply.github.com>
2023-05-06 05:12:09 +0900

    feat(nvim): change font icon

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -94,7 +94,7 @@ local function get_custom_server_configs()
 end
 
 local function config()
-	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+	local signs = { Error = "", Warn = " ", Hint = "", Info = "" }
 	for type, icon in pairs(signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
