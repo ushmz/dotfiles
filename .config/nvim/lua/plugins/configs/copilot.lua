5a48c9d6b2a78dd2e4437408af1d074031bc4965
ushmz <44394399+ushmz@users.noreply.github.com>
2023-05-10 22:10:14 +0900

    fix(nvim): resolve unintended keymap override

--- .config/nvim/lua/plugins/configs/copilot.lua
+++ .config/nvim/lua/plugins/configs/copilot.lua
@@ -3,7 +3,7 @@ local function config()
 	local key = vim.keymap.set
 	local opt = { silent = true, expr = true, script = true, replace_keycodes = false }
 
-	key("i", "<C-g>", 'copilot#Accept("<CR>")', opt)
+	key("i", "<C-g><CR>", 'copilot#Accept("")', opt)
 	key("i", "<C-j>", "<Plug>(copilot-next)")
 	key("i", "<C-k>", "<Plug>(copilot-previous)")
 	key("i", "<C-o>", "<Plug>(copilot-dismiss)")
@@ -16,5 +16,5 @@ return {
 		return vim.env.USER ~= "ushmz"
 	end,
 	config = config,
-	event = { "InsertEnter" },
+	event = { "BufRead" },
 }
