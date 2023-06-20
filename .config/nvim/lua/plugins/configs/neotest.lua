f7102394e9c11e29b8e4bf7f2a800549ac777d0d
ushmz <44394399+ushmz@users.noreply.github.com>
2023-06-20 12:59:07 +0900

    feat(nvim): set custom highlights

--- .config/nvim/lua/plugins/configs/neotest.lua
+++ .config/nvim/lua/plugins/configs/neotest.lua
@@ -96,6 +96,24 @@ local function playwright_adapter()
 	})
 end
 
+local function set_hl()
+	vim.api.nvim_set_hl(0, "NeotestPassed", { fg = "#B5BD68" })
+	vim.api.nvim_set_hl(0, "NeotestFailed", { fg = "#CC6666" })
+	vim.api.nvim_set_hl(0, "NeotestRunning", { fg = "#81A2BE" })
+	vim.api.nvim_set_hl(0, "NeotestSkipped", { fg = "#8aBEB7" })
+	vim.api.nvim_set_hl(0, "NeotestFile", { fg = "#B5BD68" })
+	vim.api.nvim_set_hl(0, "NeotestDir", { fg = "#81A2BE" })
+	vim.api.nvim_set_hl(0, "NeotestNamespace", { fg = "#CC6666" })
+	vim.api.nvim_set_hl(0, "NeotestFocused", { bold = true, underline = true })
+	vim.api.nvim_set_hl(0, "NeotestIndent", { fg = "#808080" })
+	vim.api.nvim_set_hl(0, "NeotestExpandMarker", { fg = "#81A2BE" })
+	vim.api.nvim_set_hl(0, "NeotestAdapterName", { fg = "#CC6666" })
+	vim.api.nvim_set_hl(0, "NeotestWinSelect", { fg = "#8aBEB7", bold = true })
+	vim.api.nvim_set_hl(0, "NeotestMarked", { fg = "#F0C674", bold = true })
+	vim.api.nvim_set_hl(0, "NeotestTarget", { fg = "#CC6666" })
+	vim.api.nvim_set_hl(0, "NeotestTest", { fg = "#B4B4B4" })
+end
+
 local function config()
 	-- require("neodev").setup({
 	-- 	library = { plugins = { "neotest" }, types = true },
@@ -109,6 +127,7 @@ local function config()
 			playwright_adapter(),
 		},
 	})
+	set_hl()
 end
 
 return {
