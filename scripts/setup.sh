a039782305f469c5ffeb37f09e4db61c71a7701d
ushmz <44394399+ushmz@users.noreply.github.com>
2023-06-25 23:52:55 +0900

    feat(script): fix brew setup to go with linuxbrew

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,18 +5,36 @@ function info() {
 }
 
 # Setup asdf
+if [ $(uname) == "Darwin" ]; then
+    brew install openssl readline
+else
+    sudo apt install -y \
+        autoconf \
+        bison \
+        build-essential \
+        libssl-dev \
+        libyaml-dev \
+        libreadline6-dev \
+        zlib1g-dev \
+        libncurses5-dev \
+        libffi-dev \
+        libgdbm6 \
+        libgdbm-dev \
+        libdb-dev
+fi
+
 asdf plugin add java
 asdf plugin add nodejs
 asdf plugin add python
 asdf plugin add ruby
 
 # Install them as necessary
-# asdf install nodejs 14.17.1
-# asdf install nodejs 16.17.0
-# asdf install python 3.6.15
-# asdf install python 3.8.16
-# asdf install python 3.11.2
-# asdf install ruby 3.0.3
+asdf install nodejs 14.17.1
+asdf install nodejs 16.17.0
+asdf install python 3.6.15
+asdf install python 3.8.16
+asdf install python 3.11.2
+asdf install ruby 3.0.3
 
 # Generate SSH key
 info "SSH setup"
