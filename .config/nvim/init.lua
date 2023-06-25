c0d58c07e5f4f858923506fe100e074d41d616e5
ushmz <44394399+ushmz@users.noreply.github.com>
2023-06-26 00:41:03 +0900

    feat(nvim): create autocmd only in mcaOS

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,16 @@
 vim.cmd("autocmd!")
 
 -- Auto switch IME
-vim.api.nvim_create_autocmd({ "InsertLeave", "VimEnter" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.fn.system({ "im-select", "com.apple.keylayout.ABC" })
-	end,
-})
+if jit and jit.os == "OSX" then
+	if vim.fn.executable("im-select") then
+		vim.api.nvim_create_autocmd({ "InsertLeave", "VimEnter" }, {
+			pattern = { "*" },
+			callback = function()
+				vim.fn.system({ "im-select", "com.apple.keylayout.ABC" })
+			end,
+		})
+	end
+end
 
 -- Set options
 require("basics")
