f12e8c77bce754e96cd06c8324147febca3f238c
ushmz <44394399+ushmz@users.noreply.github.com>
2023-06-25 23:51:16 +0900

    feat(zsh): use linuxbrew

--- zprofile.d/01-eval.zsh
+++ zprofile.d/01-eval.zsh
@@ -5,6 +5,8 @@ fi
 # Setting up brew PATH and environment variables
 if [ -e /opt/homebrew/bin/brew ]; then
 	eval $(/opt/homebrew/bin/brew shellenv)
+elif [ -e /home/linuxbrew/.linuxbrew/bin/brew ]; then
+	eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
 
 # cargo env
