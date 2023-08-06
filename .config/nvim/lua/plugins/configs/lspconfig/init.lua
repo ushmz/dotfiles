ba169f9206cd0d84db92cd5c2d97df1cc826e3ad
ushmz <44394399+ushmz@users.noreply.github.com>
2023-08-06 18:35:41 +0900

    refactor(nvim): extract custom server configuration (#166)

---
+++ .config/nvim/lua/plugins/configs/lspconfig/init.lua
@@ -0,0 +1,40 @@
+local function config()
+	local signs = { Error = "", Warn = " ", Hint = "", Info = "" }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
+
+	vim.diagnostic.config({
+		virtual_text = false,
+		signs = true,
+		underline = true,
+		update_in_insert = true,
+		float = { source = "if_many" },
+	})
+
+	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+		vim.lsp.diagnostic.on_publish_diagnostics,
+		{ underline = true, update_in_insert = false, virtual_text = false, severity_sort = true }
+	)
+
+	require("mason-lspconfig").setup_handlers({
+		function(server_name)
+			local conf = require("plugins.configs.lspconfig.server_config")[server_name]
+			require("lspconfig")[server_name].setup(conf)
+		end,
+	})
+end
+
+return {
+	{
+		"neovim/nvim-lspconfig",
+		event = { "InsertEnter" },
+		dependencies = {
+			-- make sure to setup neodev BEFORE lspconfig
+			{ "folke/neodev.nvim", ft = { "lua" } },
+			{ "hrsh7th/cmp-nvim-lsp", event = { "InsertEnter" } },
+		},
+		config = config,
+	},
+}
