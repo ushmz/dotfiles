f90b7925e46c0fd9f1e8827ef46cb96a462ce36e
ushmz <44394399+ushmz@users.noreply.github.com>
2023-08-12 18:53:27 +0900

    feat(nvim): add custom entry_maker for lsp references picker

--- .config/nvim/lua/plugins/configs/telescope/entry_maker.lua
+++ .config/nvim/lua/plugins/configs/telescope/entry_maker.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+---Strip the leading and trailing whitespace from a string.
+---@param str string
+---@return string # The stripped string.
+local strip = function(str)
+	return string.match(str, "^%s*(.-)%s*$")
+end
+
 ---Get the path and position from a string.
 ---@param str string The string to parse.
 ---@param sep string The separator to use.
@@ -113,6 +120,58 @@ M.create_for_live_grep = function(opts)
 				{ col, "TelescopeResultsNumber" },
 				{ " ", nil },
 				{ text, "TelescopeResultsComment" },
+			})
+		end
+		return entry
+	end
+end
+
+---Create a new entry maker for the lsp_references picker.
+---@param opts table
+---@return function
+M.create_for_lsp_references = function(opts)
+	local make_entry = require("telescope.make_entry")
+	local strings = require("plenary.strings")
+	local utils = require("telescope.utils")
+	local entry_display = require("telescope.pickers.entry_display")
+	local devicons = require("nvim-web-devicons")
+	local def_icon = devicons.get_icon("fname", { default = true })
+
+	return function(input)
+		local entry_maker = make_entry.gen_from_quickfix(opts or {})
+		local entry = entry_maker(input)
+		local displayer = entry_display.create({
+			separator = "",
+			items = {
+				{ width = strings.strdisplaywidth(def_icon) }, -- Icon
+				{ width = nil }, -- Space
+				{ width = nil }, -- Parent directory path
+				{ width = nil }, -- File name
+				{ width = nil }, -- Separator (Colon)
+				{ width = nil }, -- Matched char position (line number)
+				{ width = nil }, -- Separator (Colon)
+				{ width = nil }, -- Matched char position (column)
+				{ width = nil }, -- Space
+				{ remaining = true }, -- Matched line text
+			},
+		})
+
+		entry.display = function(et)
+			local filepath = vim.F.if_nil(et.filename, et.bufname)
+			local filename, directory_path = M.get_path_and_tail(filepath)
+			local icon, iconhl = utils.get_devicons(filename)
+
+			return displayer({
+				{ icon, iconhl },
+				{ " ", nil },
+				{ directory_path .. "/", "TelescopeResultsIdentifier" },
+				{ filename, nil },
+				{ ":", "TelescopeResultsComment" },
+				{ tostring(et.lnum), "TelescopeResultsNumber" },
+				{ ":", "TelescopeResultsComment" },
+				{ tostring(et.col), "TelescopeResultsNumber" },
+				{ " ", nil },
+				{ strip(et.text), "TelescopeResultsComment" },
 			})
 		end
 		return entry
