9a9d84866d93363a96371440e6a239ea6a156823
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-01 01:37:39 +0900

    feat(nvim): add cspell options (#184)

--- .config/nvim/lua/plugins/configs/lint.lua
+++ .config/nvim/lua/plugins/configs/lint.lua
@@ -1,20 +1,38 @@
 local function config()
+	local cspell = require("lint").linters.cspell
+	cspell.args = {
+		"lint",
+		"--no-color",
+		"--no-progress",
+		"--no-summary",
+		"--show-suggestions",
+	}
+
 	require("lint").linters_by_ft = {
 		css = { "stylelint" },
 		go = { "golangcilint" },
-		javascript = { "eslint" },
-		javascriptreact = { "eslint" },
+		javascript = { "eslint_d" },
+		javascriptreact = { "eslint_d" },
 		json = { "jsonlint" },
 		lua = { "luacheck" },
 		markdown = { "markdownlint" }, -- "vale"
 		python = { "flake8" },
 		rust = { "cargo" },
 		sh = { "shellcheck" },
-		typescript = { "eslint" },
-		typescriptreact = { "eslint" },
+		typescript = { "eslint_d" },
+		typescriptreact = { "eslint_d" },
 		vue = { "eslint" },
 		yaml = { "yamllint" },
 	}
+
+	vim.api.nvim_create_autocmd({ "InsertLeave", "BufReadPost", "TextChanged", "BufWritePost" }, {
+		callback = function()
+			local linters = require("lint").linters_by_ft[vim.bo.filetype]
+			if linters then
+				require("lint").try_lint("cspell")
+			end
+		end,
+	})
 end
 
 return {
