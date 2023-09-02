bb5a2ea795cf2003803333a57457a02bd7217a16
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-02 23:41:16 +0900

    feat(nvim): turn off git blame with virtual text (#193)

--- .config/nvim/lua/plugins/configs/gitsigns.lua
+++ .config/nvim/lua/plugins/configs/gitsigns.lua
@@ -17,9 +17,9 @@ local function config()
 			follow_files = true,
 		},
 		attach_to_untracked = true,
-		current_line_blame = true,
+		current_line_blame = false,
 		current_line_blame_opts = {
-			virt_text = true,
+			virt_text = false,
 			virt_text_pos = "eol",
 			delay = 1000,
 			ignore_whitespace = false,
