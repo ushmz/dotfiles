7df0a0634e8e9488e41438b6070b25776d16de67
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-02 21:58:25 +0900

    feat(nvim): update config (vim-illuminate)

--- .config/nvim/lua/plugins/configs/illuminate.lua
+++ .config/nvim/lua/plugins/configs/illuminate.lua
@@ -2,6 +2,18 @@ return {
 	"RRethy/vim-illuminate",
 	event = { "CursorHold" },
 	config = function()
+		require("illuminate").configure({
+			filetypes_denylist = {
+				"cmp_docs",
+				"cmp_menu",
+				"dirvish",
+				"fugitive",
+				"lazy",
+				"noice",
+				"prompt",
+				"TelescopePrompt",
+			},
+		})
 		vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "CursorLine" })
 		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "CursorLine" })
 		-- vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "CursorLine"})
