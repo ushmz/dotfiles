ecf69177aa8a1c8c18213d92babcf4843616d62e
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-02 16:33:12 +0900

    refactor(nvim): simplify lsp config

--- .config/nvim/lua/plugins/configs/lspconfig/hl.lua
+++ .config/nvim/lua/plugins/configs/lspconfig/hl.lua
@@ -9,6 +9,12 @@ M.set = function()
 	set_hl("DiagnosticWarn", { fg = theme.yellow })
 	set_hl("DiagnosticHint", { fg = theme.cyan })
 	set_hl("DiagnosticInfo", { fg = theme.green })
+
+	local signs = { Error = "", Warn = " ", Hint = "", Info = "" }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
 end
 
 return M
