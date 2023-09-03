391fa25c4b4a9416e7e8751c80f69b927311ea8d
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-04 00:11:08 +0900

    feat(nvim): use default key mappings

--- .config/nvim/lua/plugins/configs/treesj.lua
+++ .config/nvim/lua/plugins/configs/treesj.lua
@@ -1,19 +1,8 @@
 return {
 	"Wansmer/treesj",
-	keys = { "<C-s>m", "<C-s>M", "<C-s>j", "<C-s>s" },
+	keys = { "<space>m", "<space>j", "<space>s" },
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
-		require("treesj").setup({
-			use_default_keymaps = false,
-		})
-
-		-- It works with dot
-		vim.keymap.set("n", "<C-s>m", require("treesj").toggle)
-		-- It does't work with dot
-		vim.keymap.set("n", "<C-s>M", function()
-			require("treesj").toggle({ split = { recursive = true } })
-		end)
-		vim.keymap.set("n", "<C-s>j", require("treesj").join)
-		vim.keymap.set("n", "<C-s>s", require("treesj").split)
+		require("treesj").setup({})
 	end,
 }
