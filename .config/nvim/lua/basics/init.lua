b42261122cdfdd2a4b727bc137e9d4a9a3a97f52
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-11 03:18:50 +0900

    feat(nvim): add disable builtin plugin config

--- .config/nvim/lua/basics/init.lua
+++ .config/nvim/lua/basics/init.lua
@@ -15,33 +15,6 @@ vim.cmd("set fo+=rqnM1j")
 
 vim.api.nvim_set_var("netrw_home", home .. ".local/state/vim")
 
-g.loaded_2html_plugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_gzip = 1
-g.loaded_logiPat = 1
-g.loaded_man = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_netrwFileHandlers = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
-g.loaded_remote_plugins = 1
-g.loaded_rplugin = 1
-g.loaded_rrhelper = 1
-g.loaded_shada_plugin = 1
-g.loaded_shada_plugin = 1
-g.loaded_spec = 1
-g.loaded_spellfile_plugin = 1
-g.loaded_spellfile_plugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_tutor_mode_plugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-
 g.load_black = 1
 g.loaded_fzf = 1
 g.loaded_gtags = 1
