74842f4c2c009d77dc083540391edaaa037ef25a
ushmz <44394399+ushmz@users.noreply.github.com>
2023-09-16 22:35:00 +0900

    feat(nvim): update shell prompt (remove starship)

--- zshrc.d/01-prompt.zsh
+++ zshrc.d/01-prompt.zsh
@@ -1,13 +1,28 @@
 bindkey -e
 
-# starship (cross-shell prompt)
-if type starship &>/dev/null; then
-	eval "$(starship init zsh)"
-else
-	setopt prompt_subst
-	precmd_refresh_prompt() {
-		export PS1=$'\n'"%F{green}%n%f%F{blue}@%f%F{green}%m%f:%~"$'\n'$(if [[ $? -eq 0 ]]; then echo %F{white}\>\>\> %f; else echo %F{red}\>\>\> %f; fi)
-	}
-	autoload -Uz add-zsh-hook
-	add-zsh-hook precmd precmd_refresh_prompt
-fi
+get_cwd() {
+    local branch
+    branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+    if [[ -n $branch ]]; then
+        echo "%B%F{blue}%C%f%b on %B%F{magenta} ${branch}%f%b"
+    else
+        echo "%B%F{blue}%~%f%b"
+    fi
+}
+
+get_prompt() {
+    local propmt
+    if [[ $? -eq 0 ]]; then
+        echo "%F{blue}❯%f%F{yellow}❯%f%F{white}❯ %f";
+    else
+        echo "%F{red}❯❯❯ %f";
+    fi
+}
+
+precmd_refresh_prompt() {
+    export PS1=$'\n'"$(get_cwd)"$'\n'"$(get_prompt)"
+}
+
+setopt prompt_subst
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd precmd_refresh_prompt
