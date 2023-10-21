b4b5497423025854af871e35f6a02c482418bf55
ushmz <44394399+ushmz@users.noreply.github.com>
2023-10-21 22:21:52 +0900

    feat(nvim): add keymap desciprtion

--- .config/nvim/lua/plugins/configs/smart-splits.lua
+++ .config/nvim/lua/plugins/configs/smart-splits.lua
@@ -33,21 +33,23 @@ local function config()
 		log_level = "info",
 	})
 
+  -- stylua: ignore start
 	-- resizing splits (these keymaps will also accept a range)
-	vim.keymap.set("n", "<Left>", require("smart-splits").resize_left)
-	vim.keymap.set("n", "<Down>", require("smart-splits").resize_down)
-	vim.keymap.set("n", "<Up>", require("smart-splits").resize_up)
-	vim.keymap.set("n", "<Right>", require("smart-splits").resize_right)
+	vim.keymap.set("n", "<Left>", require("smart-splits").resize_left, { desc = "smart-split: Resize left" })
+	vim.keymap.set("n", "<Down>", require("smart-splits").resize_down, { desc = "smart-split: Split: Resize down" })
+	vim.keymap.set("n", "<Up>", require("smart-splits").resize_up, { desc = "smart-split: Resize up" })
+	vim.keymap.set("n", "<Right>", require("smart-splits").resize_right, { desc = "smart-split: Resize right" })
 	-- moving between splits
-	vim.keymap.set("n", "<C-w>h", require("smart-splits").move_cursor_left)
-	vim.keymap.set("n", "<C-w>j", require("smart-splits").move_cursor_down)
-	vim.keymap.set("n", "<C-w>k", require("smart-splits").move_cursor_up)
-	vim.keymap.set("n", "<C-w>l", require("smart-splits").move_cursor_right)
+	vim.keymap.set("n", "<C-w>h", require("smart-splits").move_cursor_left, { desc = "smart-split: Move cursor left" })
+	vim.keymap.set("n", "<C-w>j", require("smart-splits").move_cursor_down, { desc = "smart-split: Move cursor down" })
+	vim.keymap.set("n", "<C-w>k", require("smart-splits").move_cursor_up, { desc = "smart-split: Move cursor up" })
+	vim.keymap.set("n", "<C-w>l", require("smart-splits").move_cursor_right, { desc = "smart-split: Move cursor right" })
 	-- swapping buffers between windows
-	vim.keymap.set("n", "<C-w>H", require("smart-splits").swap_buf_left)
-	vim.keymap.set("n", "<C-w>J", require("smart-splits").swap_buf_down)
-	vim.keymap.set("n", "<C-w>K", require("smart-splits").swap_buf_up)
-	vim.keymap.set("n", "<C-w>L", require("smart-splits").swap_buf_right)
+	vim.keymap.set("n", "<C-w>H", require("smart-splits").swap_buf_left, { desc = "smart-split: Swap buffer left" })
+	vim.keymap.set("n", "<C-w>J", require("smart-splits").swap_buf_down, { desc = "smart-split: Swap buffer down" })
+	vim.keymap.set("n", "<C-w>K", require("smart-splits").swap_buf_up, { desc = "smart-split: Swap buffer up" })
+	vim.keymap.set("n", "<C-w>L", require("smart-splits").swap_buf_right, { desc = "smart-split: Swap buffer right" })
+	-- stylua: ignore end
 end
 
 return {
