18b410840e141cdee585882a4a1a5a2a1939936b
ushmz <44394399+ushmz@users.noreply.github.com>
2023-10-21 23:01:27 +0900

    feat(nvim): plugin major update

--- .config/nvim/lua/plugins/configs/indent-blankline.lua
+++ .config/nvim/lua/plugins/configs/indent-blankline.lua
@@ -1,17 +1,10 @@
 local function config()
-	vim.opt.list = true
-	-- vim.opt.listchars:append "space:⋅"
-	-- vim.opt.listchars:append("eol:↴")
-	require("indent_blankline").setup({
-		show_end_of_line = true,
-		space_char_blankline = " ",
-		show_current_context = true,
-		show_current_context_start = true,
-	})
+	require("ibl").setup({ scope = { show_exact_scope = true } })
 end
 
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	event = { "BufNewFile", "BufRead" },
 	config = config,
 }
