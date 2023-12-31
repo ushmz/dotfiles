9668c179fd5f208150dd197a4f3b7338623364db
ushmz <44394399+ushmz@users.noreply.github.com>
2023-12-31 16:54:49 +0900

    feat: dim inactive buffer and pane (#232)
    * feat(nvim): dim inactive buffer
    
    * feat(tmux): dim inactive pane

--- .config/nvim/lua/plugins/configs/tint.lua
+++ .config/nvim/lua/plugins/configs/tint.lua
@@ -3,6 +3,13 @@ return {
 	event = { "BufRead" },
 	config = function()
 		require("tint").setup({
+			tint = -60,
+			saturation = 0.4,
+			highlight_ignore_patterns = {
+				"WinSeparator",
+				"Status.*",
+				"Rainbow*",
+			},
 			window_ignore_function = function(win_id)
 				local buf_id = vim.api.nvim_win_get_buf(win_id)
 				local buftype = vim.api.nvim_buf_get_option(buf_id, "buftype")
