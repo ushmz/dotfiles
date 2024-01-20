d69055de00752a86345d6ebf023d222fa02dc4a0
ushmz <44394399+ushmz@users.noreply.github.com>
2024-01-21 04:11:16 +0900

    feat(nvim): set some plugins as a workspace library

--- .config/nvim/lua/plugins/configs/neodev.lua
+++ .config/nvim/lua/plugins/configs/neodev.lua
@@ -8,11 +8,13 @@ return {
 				runtime = true,
 				types = true,
 				plugins = true,
-				-- you can also specify the list of plugins to make available as a workspace library
-				-- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
 			},
 			setup_jsonls = true,
-			override = function(root_dir, options) end,
+			override = function(_, options)
+				options.enabled = true
+				-- Use treesitter and plenary.nvim as a workspace library
+				options.plugins = { "nvim-treesitter", "plenary.nvim" }
+			end,
 			lspconfig = true,
 			pathStrict = true,
 		})
