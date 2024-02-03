eca5ceb1d04e42b1097a7fd583482fe9c73a44c8
ushmz <44394399+ushmz@users.noreply.github.com>
2024-02-04 02:06:58 +0900

    feat(nvim): update LuaLS config for Neovim Lua

--- .config/nvim/lua/plugins/configs/lspconfig/server_config.lua
+++ .config/nvim/lua/plugins/configs/lspconfig/server_config.lua
@@ -71,8 +71,21 @@ config["lua_ls"] = vim.tbl_deep_extend("force", default_config, {
 		Lua = {
 			diagnostics = { globals = { "vim" } },
 			telemetry = { enable = false },
+			runtime = {
+				version = "LuaJIT",
+				pathStrict = true,
+				path = { "?.lua", "?/init.lua" },
+			},
 			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
+					vim.fn.stdpath("config") .. "/lua",
+					-- vim.fn.*, vim.api.*
+					vim.env.VIMRUNTIME .. "/lua",
+					-- vim.uv.*, (vim.loop.*)
+					"${3rd}/luv/library",
+					"${3rd}/busted/library",
+					"${3rd}/luaassert/library",
+				}),
 				checkThirdParty = false,
 			},
 		},
