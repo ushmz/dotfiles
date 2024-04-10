caf32961fc0d6448a531333931d9fa7e264f2683
ushmz <44394399+ushmz@users.noreply.github.com>
2024-04-11 01:16:48 +0900

    chore(scripts): prettify logs (#269)

--- scripts/brewfile.sh
+++ scripts/brewfile.sh
@@ -2,13 +2,8 @@
 set -Cue
 
 if ! (type brew 1>/dev/null); then
-    echo 'command brew not found' >&2
-    exit 1
-fi
-
-if ! (type mas 1 >/dev/null); then
-    echo 'command mas not found' >&2
-    exit 1
+	printf '\033[1m[ERROR]\033[31m command `brew` not found.\n' >&2
+	exit 1
 fi
 
 # brew tap
@@ -21,4 +16,9 @@ brew leaves | sed -r "s/(.*)/brew \'\1\'/"
 brew list --cask | sed -r "s/(.*)/cask \'\1\'/"
 
 # mas
+if ! (type mas 1>/dev/null); then
+	printf '\033[1m\033[33m[WARN]\033[0m command `mas` not found. Skipped.\n' >&2
+	exit 0
+fi
+
 mas list | sed -r 's/\ {2,}/\t/g' | sed -r "s/(.+)\t(.+)\t.*/mas '\2', id: \1/"
