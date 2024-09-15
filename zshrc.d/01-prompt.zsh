f09e502b74444893f55be4ed26d58c6fdffb235d
ushmz <44394399+ushmz@users.noreply.github.com>
2024-09-16 01:57:36 +0900

    feat(zsh): enable starship (#303)

--- zshrc.d/01-prompt.zsh
+++ zshrc.d/01-prompt.zsh
@@ -1,28 +1,6 @@
 bindkey -e
 
-get_cwd() {
-    local branch
-    branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-    if [[ -n $branch ]]; then
-        echo "%B%F{blue}%C%f%b on %B%F{magenta} ${branch}%f%b"
-    else
-        echo "%B%F{blue}%~%f%b"
-    fi
-}
-
-get_prompt() {
-    local propmt
-    if [[ $? -eq 0 ]]; then
-        echo "%F{blue}❯%f%F{yellow}❯%f%F{white}❯ %f";
-    else
-        echo "%F{red}❯❯❯ %f";
-    fi
-}
-
-precmd_refresh_prompt() {
-    export PS1=$'\n'"$(get_cwd)"$'\n'"$(get_prompt)"
-}
-
 setopt prompt_subst
 autoload -Uz add-zsh-hook
-add-zsh-hook precmd precmd_refresh_prompt
+
+eval "$(starship init zsh)"
