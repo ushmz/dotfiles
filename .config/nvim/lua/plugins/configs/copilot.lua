7614d775603001d6a08ddb01e0cc23de41082d21
ushmz <44394399+ushmz@users.noreply.github.com>
2025-01-23 02:01:30 +0900

    feat(nvim): use copilot.lua

--- .config/nvim/lua/plugins/configs/copilot.lua
+++ .config/nvim/lua/plugins/configs/copilot.lua
@@ -1,20 +1,52 @@
-local function config()
-	vim.g.copilot_no_tab_map = true
-	local key = vim.keymap.set
-	local opt = { silent = true, expr = true, script = true, replace_keycodes = false }
-
-	key("i", "<C-g><CR>", 'copilot#Accept("")', opt)
-	key("i", "<C-g>j", "<Plug>(copilot-next)")
-	key("i", "<C-g>k", "<Plug>(copilot-previous)")
-	key("i", "<C-g>e", "<Plug>(copilot-dismiss)")
-	key("i", "<C-g>s", "<Plug>(copilot-suggest)")
+local config = function()
+	require("copilot").setup({
+		panel = {
+			enabled = true,
+			auto_refresh = false,
+			keymap = {
+				jump_prev = "[[",
+				jump_next = "]]",
+				accept = "<CR>",
+				refresh = "gr",
+				open = "<M-CR>",
+			},
+			layout = {
+				position = "bottom", -- | top | left | right | horizontal | vertical
+				ratio = 0.4,
+			},
+		},
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+			hide_during_completion = true,
+			debounce = 75,
+			keymap = {
+				accept = "<C-g><CR>",
+				accept_word = "<C-g>w",
+				accept_line = false,
+				next = "<C-g><C-n>",
+				prev = "<C-g><C-p>",
+				dismiss = "<C-g>e",
+			},
+		},
+		filetypes = {
+			yaml = false,
+			markdown = false,
+			help = false,
+			gitcommit = false,
+			gitrebase = false,
+			hgcommit = false,
+			svn = false,
+			cvs = false,
+		},
+		copilot_node_command = "node", -- Node.js version must be > 18.x
+		server_opts_overrides = {},
+	})
 end
 
 return {
-	"github/copilot.vim",
-	cond = function()
-		return vim.env.USER ~= "ushmz"
-	end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
 	config = config,
-	event = { "InsertEnter" },
 }
