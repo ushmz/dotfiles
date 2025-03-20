927b736a431263fa246fad1d5896bd4fcb2a7536
ushmz <44394399+ushmz@users.noreply.github.com>
2025-03-21 02:16:45 +0900

    feat(nvim): fix key conflict

--- .config/nvim/lua/plugins/configs/nvim-bqf.lua
+++ .config/nvim/lua/plugins/configs/nvim-bqf.lua
@@ -1,4 +1,12 @@
 return {
 	"kevinhwang91/nvim-bqf",
 	event = { "VeryLazy" },
+	config = function()
+		require("bqf").setup({
+			auto_enable = true,
+			func_map = {
+				vsplit = "",
+			},
+		})
+	end,
 }
