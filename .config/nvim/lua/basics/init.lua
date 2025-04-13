f4de13def65f53e30fdb989bd045e50850d14ddb
ushmz <44394399+ushmz@users.noreply.github.com>
2025-04-13 09:45:13 +0900

    feat(nvim): keymap for copying filename and basename of current buffer (#327)
    * feat(nvim): copy filename/basename on current buffer
    
    * feat(nvim): highlight yanked words

--- .config/nvim/lua/basics/init.lua
+++ .config/nvim/lua/basics/init.lua
@@ -102,3 +102,11 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		end
 	end,
 })
+
+-- Highlight yanked word
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({})
+	end,
+})
