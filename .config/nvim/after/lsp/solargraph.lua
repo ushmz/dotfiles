669149b60750be5f06248103075cb2c600b309a8
ushmz <44394399+ushmz@users.noreply.github.com>
2025-06-02 00:23:06 +0900

    wip: setup ruby

---
+++ .config/nvim/after/lsp/solargraph.lua
@@ -0,0 +1,17 @@
+---@type vim.lsp.Config
+return {
+	settings = {
+		solargraph = {
+			autoformat = false,
+			completion = true,
+			definitions = true,
+			diagnostics = true,
+			folding = true,
+			formatting = false,
+			highlights = true,
+			references = true,
+			rename = true,
+			symbols = true,
+		},
+	},
+}
