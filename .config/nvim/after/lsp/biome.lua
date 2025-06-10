592d2cc173b846c16e89871fb4a161bf59cd5d2e
ushmz <44394399+ushmz@users.noreply.github.com>
2025-06-10 20:23:06 +0900

    feat(nvim): apply fixAll code action on save

--- .config/nvim/after/lsp/biome.lua
+++ .config/nvim/after/lsp/biome.lua
@@ -1,3 +1,23 @@
+---@param client vim.lsp.Client
+---@param bufnr number
+---@param cmd string
+local function exec_code_action_sync(client, bufnr, cmd)
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { cmd }, diagnostics = {} }
+	params.range = cmd.range
+
+	-- NOTE: Use `request_sync` of `vim.lsp.Client` instead of `vim.lsp.buf_request_sync` to filter target language server
+	local res = client.request_sync("textDocument/codeAction", params, 5000, bufnr)
+	if res and res.result then
+		for _, r in ipairs(res.result) do
+			if r.edit then
+				local encoding = vim.lsp.get_client_by_id(r.edit.client_id).offset_encoding or "utf-8"
+				vim.lsp.util.apply_workspace_edit(r.edit, encoding)
+			end
+		end
+	end
+end
+
 ---@type vim.lsp.Config
 return {
 	root_markers = { "biome.json", "biome.jsonc" },
@@ -11,4 +31,16 @@ return {
 		end
 	end,
 	single_file_support = true,
+	on_attach = function(client, bufnr)
+		local fmtag = vim.api.nvim_create_augroup("LspDocumentFormatting", {})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			group = fmtag,
+			callback = function(args)
+				vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 5000 })
+				-- TODO : execute "textDocument/codeAction" to apply the fix
+				exec_code_action_sync(client, bufnr, "source.fixAll")
+			end,
+		})
+	end,
 }
