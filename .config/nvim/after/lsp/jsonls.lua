e0b920ba3b872015b411d842e2a3ed7768f1df82
ushmz <44394399+ushmz@users.noreply.github.com>
2025-10-08 20:23:13 +0900

    chore(nvim): tiny fix for Neovim LSP (#356)
    * feat(nvim): disable jsonls formatting
    
    * feat(nvim): optimize tsserver config for monorepo (wip)

--- .config/nvim/after/lsp/jsonls.lua
+++ .config/nvim/after/lsp/jsonls.lua
@@ -1,5 +1,9 @@
 ---@type vim.lsp.Config
 return {
+	on_attach = function(client, _)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+	end,
 	settings = {
 		json = {
 			schemas = {
