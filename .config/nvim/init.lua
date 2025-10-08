991f0851347a63ec586ae9d4444c59d0a69e3b11
ushmz <44394399+ushmz@users.noreply.github.com>
2025-10-08 19:32:13 +0900

    feat(nvim): switch IME on FocusGained event except insert mode (#352)
    * feat: add FocusGained event to IME switch
    
    * feat: switch to ABC when focus gained except insert mode
    
    * style: fmt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,9 +3,9 @@ vim.cmd("autocmd!")
 require("basics")
 
 if vim.g.vscode then
-  require("basics.vscode")
-  require("mappings.vscode")
-  return
+	require("basics.vscode")
+	require("mappings.vscode")
+	return
 end
 
 require("mappings")
@@ -15,6 +15,16 @@ require("plugins")
 -- Auto switch IME
 if jit and jit.os == "OSX" then
 	if vim.fn.executable("im-select") then
+		vim.api.nvim_create_autocmd("FocusGained", {
+			pattern = { "*" },
+			callback = function()
+				if vim.fn.mode() == "i" then
+					return
+				end
+				vim.fn.system({ "im-select", "com.apple.keylayout.ABC" })
+			end,
+		})
+
 		vim.api.nvim_create_autocmd({ "InsertLeave", "VimEnter" }, {
 			pattern = { "*" },
 			callback = function()
