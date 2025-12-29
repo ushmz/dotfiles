49fd02a14a63ebbddcd40e1cd159bd01dff6f18e
ushmz <44394399+ushmz@users.noreply.github.com>
2025-12-29 21:52:29 +0900

    chore: cleanup (#365)
    * chore: lockfile
    
    * feat: add css lsp configs
    
    * feat: add tabi.nvim
    
    * style: fmt

--- .config/nvim/lua/plugins/configs/js-teleporter.lua
+++ .config/nvim/lua/plugins/configs/js-teleporter.lua
@@ -1,14 +1,14 @@
 local function config()
-	require("js-teleporter").setup({
-		test_roots = { "__test__", "__tests__", "test", "tests" },
-	})
+  require("js-teleporter").setup({
+    test_roots = { "__test__", "__tests__", "test", "tests" },
+  })
 end
 
 return {
-	"ushmz/js-teleporter.nvim",
-	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-	},
-	config = config,
+  "ushmz/js-teleporter.nvim",
+  -- ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+  },
+  config = config,
 }
---
+++ .config/nvim/lua/plugins/configs/tabi.lua
@@ -0,0 +1,14 @@
+return {
+	dir = "/Users/ushmz/src/github.com/ushmz/tabi.nvim",
+	lazy = false,
+	---@type TabiOptions
+	opts = {
+		ui = {
+			selector = "telescope",
+			note_preview_length = 80,
+		},
+    keymaps = {
+      enabled = false
+    }
+	},
+}
