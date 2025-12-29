bfe6920626bea60cf998bccfc5a2ff66cc48b6cc
ushmz <44394399+ushmz@users.noreply.github.com>
2025-12-29 23:57:41 +0900

    chore: disable repeat move

--- .config/nvim/lua/plugins/configs/treesitter/textobj/repeat.lua
+++ .config/nvim/lua/plugins/configs/treesitter/textobj/repeat.lua
@@ -3,8 +3,8 @@ local M = {}
 M.setup = function()
 	local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
 
-	vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-	vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+	-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+	-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
 	vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
 	vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
