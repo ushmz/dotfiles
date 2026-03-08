b7b780e1334848604bea5f74b00f4d80dcabd3a0
ushmz <44394399+ushmz@users.noreply.github.com>
2026-03-09 00:11:16 +0900

    wip: add codecompanion.nvim plugin config

---
+++ .config/nvim/lua/plugins/configs/codecompanion.lua
@@ -0,0 +1,10 @@
+return {
+  "olimorris/codecompanion.nvim",
+  cond = false,
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {},
+}
