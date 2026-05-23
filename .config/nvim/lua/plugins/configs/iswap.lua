7df408593ad277a9f9dea90850b3cbe833936c88
ushmz <44394399+ushmz@users.noreply.github.com>
2026-05-24 02:09:03 +0900

    chore: rm unused plugins

--- .config/nvim/lua/plugins/configs/iswap.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "mizlan/iswap.nvim",
-  pin = true,
-  commit = "d828a3adb2e21d4fc0b264b55059402aaff1f08a",
-  cmd = { "ISwap" },
-}
--- .config/nvim/lua/plugins/configs/plantuml-syntax.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "aklt/plantuml-syntax",
-  pin = true,
-  commit = "9d4900aa16674bf5bb8296a72b975317d573b547",
-  event = { "BufNewFile", "BufRead" },
-}
