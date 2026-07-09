846c23cb43fad6b5c6346bc0365d01ef2dec6f9e
ushmz <44394399+ushmz@users.noreply.github.com>
2026-07-09 20:56:54 +0900

    chore: Switch bigfile.nvim to local fork with explicit features
    Point at a local checkout for iterating on it directly, and load
    eagerly with an explicit feature list (including a treesitter
    override that defers stopping the parser to FileType) instead of
    relying on the plugin's VeryLazy default.

--- .config/nvim/lua/plugins/configs/bigfile.lua
+++ .config/nvim/lua/plugins/configs/bigfile.lua
@@ -1,6 +1,33 @@
 return {
-  "LunarVim/bigfile.nvim",
+  -- "LunarVim/bigfile.nvim",
+  dir = "/Users/ushmz/src/github.com/ushmz/bigfile.nvim",
   pin = true,
   commit = "33eb067e3d7029ac77e081cfe7c45361887a311a",
-  event = { "VeryLazy" },
+  lazy = false,
+  config = function()
+    require("bigfile").setup({
+      features = {
+        "indent_blankline",
+        "illuminate",
+        "lsp",
+        {
+          name = "treesitter",
+          disable = function(buf)
+            vim.api.nvim_create_autocmd("FileType", {
+              buffer = buf,
+              once = true,
+              callback = function()
+                vim.treesitter.stop(buf)
+              end,
+            })
+          end,
+          opts = { defer = true },
+        },
+        "syntax",
+        "matchparen",
+        "vimopts",
+        "filetype",
+      },
+    })
+  end,
 }
